32d89b604dabaab69e8479bb3d882ce4e22a2426
Abhi Singh <156217394+ShahinaAbhi143@users.noreply.github.com>
2024-10-20 15:07:37 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -95,7 +95,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/imagine_iq",
+    "https://github.com/TheTeamVivek/YukkiMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
