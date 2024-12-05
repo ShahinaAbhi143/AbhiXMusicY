249d743c1318ca9b8659efea2a2b511bf14a4442
Abhi Singh <156217394+ShahinaAbhi143@users.noreply.github.com>
2024-12-05 16:22:08 +0530

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
 #
 # This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
+# and is released under the MIT License.
 # Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
 #
 # All rights reserved.
@@ -11,7 +11,7 @@ import asyncio
 import sys
 
 from pyrogram import Client
-
+from pyrogram.errors import ChatWriteForbidden
 import config
 
 from ..logging import LOGGER
@@ -32,6 +32,7 @@ class Userbot(Client):
                 api_id=config.API_ID,
                 api_hash=config.API_HASH,
                 in_memory=True,
+                no_updates=True,
                 session_string=session.strip(),
             )
             self.clients.append(client)
@@ -41,7 +42,17 @@ class Userbot(Client):
         try:
             await client.start()
             assistants.append(index)
-            await client.send_message(config.LOG_GROUP_ID, "Assistant Started")
+            try:
+                await client.send_message(config.LOG_GROUP_ID, "Assistant Started")
+            except ChatWriteForbidden:
+                try:
+                    await client.join_chat(config.LOG_GROUP_ID)
+                    await client.send_message(config.LOG_GROUP_ID, "Assistant Started")
+                except Exception:
+                    LOGGER(__name__).error(
+                        f"Assistant Account {index} has failed to send message in Loggroup Make sure you have added assistsant in Loggroup."
+                    )
+                    sys.exit(1)
 
             get_me = await client.get_me()
             client.username = get_me.username
